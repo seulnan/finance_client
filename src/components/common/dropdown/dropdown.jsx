43160fb50738b607d4e34b9a887bd54a2f208d9f
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2025-01-25 20:51:03 +0900

    [Add] dropdown 디자인 추가

--- src/components/common/dropdown/dropdown.jsx
+++ src/components/common/dropdown/dropdown.jsx
@@ -1,16 +1,29 @@
 import React from "react";
+import "./dropdown.css";
+import dropdownIcon from "../../../assets/images/dropdownIcon.png";
 
 function Dropdown({ label, options, value, onChange }) {
   return (
     <div className="dropdown">
       <label className="dropdownLabel">{label}</label>
-      <select className="dropdownSelect" value={value} onChange={(e) => onChange(e.target.value)}>
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </select>
+      <div className="dropdownWrapper">
+        <select
+          className="dropdownSelect"
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+        >
+          {options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+        <img
+          src= {dropdownIcon}
+          alt="Dropdown Icon"
+          className="dropdownIcon"
+        />
+      </div>
     </div>
   );
 }
