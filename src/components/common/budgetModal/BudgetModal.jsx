46d869d6b614fd43f7ab8957e0adee3250b954c9
Huniiiiiii <rtg8464@gmail.com>
2025-02-01 20:54:00 +0900

    [UI]색 드롭다운 색 추가
    add와 edit에서 색 선택하는 드롭다운에 동그라미 색 미리보기 생성 완료

--- src/components/common/budgetModal/BudgetModal.jsx
+++ src/components/common/budgetModal/BudgetModal.jsx
@@ -3,6 +3,8 @@ import baseAxios from "../../../baseAxios.js";
 import SearchField from "../searchField/SearchField.jsx";
 import "../budgetModal/BudgetModal.css";
 
+import Dropdowncross from '../../../assets/images/dropdowncross.svg';
+
 const BudgetModal = ({ type, budget, onClose, onSuccess }) => {
   const [formData, setFormData] = useState({
     category: "",
@@ -15,6 +17,7 @@ const BudgetModal = ({ type, budget, onClose, onSuccess }) => {
     availableColors: [],
     usedColors: [],
   });
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
   useEffect(() => {
     if (type === "add") {
@@ -41,6 +44,11 @@ const BudgetModal = ({ type, budget, onClose, onSuccess }) => {
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
+  };
+
+  const handleThemeSelection = (color) => {
+    setFormData({ ...formData, theme: color });
+    setDropdownOpen(false);
   };
 
   const handleSubmit = async () => {
@@ -85,6 +93,7 @@ const BudgetModal = ({ type, budget, onClose, onSuccess }) => {
           </h2>
           <button className="CloseButton" onClick={onClose}></button>
         </div>
+
         {type === "add" && (
           <>
             <p>
@@ -93,68 +102,21 @@ const BudgetModal = ({ type, budget, onClose, onSuccess }) => {
             </p>
             <div className="FormGroup">
               <label>Budget Category</label>
-              <select
-                name="category"
-                onChange={handleInputChange}
-                value={formData.category}
-              >
+              <select name="category" onChange={handleInputChange} value={formData.category}>
                 <option value="">Select Category</option>
-                {options.availableCategories.map((category) => (
-                  <option key={category} value={category}>
-                    {category}
-                  </option>
-                ))}
                 {options.usedCategories.map((category) => (
                   <option key={category} value={category} disabled>
                     {category} (Already Used)
                   </option>
                 ))}
-              </select>
-            </div>
-            <div className="FormGroup">
-              <label>Maximum Spend</label>
-              <SearchField
-                type="icon-left"
-                placeholder="e.g. 2000"
-                onChange={(e) =>
-                  setFormData({ ...formData, limit: e.target.value })
-                }
-              />
-            </div>
-            <div className="FormGroup">
-              <label>Theme</label>
-              <select
-                name="theme"
-                onChange={handleInputChange}
-                value={formData.theme}
-              >
-                <option value="">Select Theme</option>
-                {options.usedColors.map((color) => (
-                  <option key={`used-${color}`} value={color} disabled>
-                    {color} (Already Used)
-                  </option>
-                ))}
-                {options.availableColors.map((color) => (
-                  <option key={`available-${color}`} value={color}>
-                    {color}
+                {options.availableCategories.map((category) => (
+                  <option key={category} value={category}>
+                    {category}
                   </option>
                 ))}
               </select>
             </div>
-            <button className="ModalSubmitButton" onClick={handleSubmit}>
-              Add Budget
-            </button>
-          </>
-        )}
-        {type === "edit" && (
-          <>
-            <p>
-              As your budgets change, feel free to update your spending limits.
-            </p>
-            <div className="FormGroup">
-              <label>Budget Category</label>
-              <input type="text" value={formData.category} disabled />
-            </div>
+
             <div className="FormGroup">
               <label>Maximum Spend</label>
               <SearchField
@@ -166,43 +128,125 @@ const BudgetModal = ({ type, budget, onClose, onSuccess }) => {
                 }
               />
             </div>
+
             <div className="FormGroup">
               <label>Theme</label>
-              <select
-                name="theme"
-                onChange={handleInputChange}
-                value={formData.theme}
-              >
-                <option value="">Select Theme</option>
-                {options.usedColors.map((color) => (
-                  <option key={`used-${color}`} value={color} disabled>
-                    {color} (Already Used)
-                  </option>
-                ))}
-                {options.availableColors.map((color) => (
-                  <option key={`available-${color}`} value={color}>
-                    {color}
-                  </option>
-                ))}
-              </select>
+              <div className="ColorDropdown">
+                <div className="DropdownHeader">
+                  <div onClick={() => setDropdownOpen(!dropdownOpen)} style={{ display: "flex", alignItems: "center", width: "100%" }}>
+                    {formData.theme ? (
+                      <>
+                        <span className="ColorCircle" style={{ backgroundColor: formData.theme }}></span>
+                        {formData.theme}
+                      </>
+                    ) : (
+                      "Select Theme"
+                    )}
+                  </div>
+                  <img 
+                    src={Dropdowncross} 
+                    alt="Toggle Dropdown" 
+                    className={`DropdownCloseIcon ${dropdownOpen ? "visible" : "hidden"}`} 
+                    onClick={() => setDropdownOpen(!dropdownOpen)}
+                  />
+                </div>
+                
+                {dropdownOpen && (
+                  <div className="DropdownList">
+                    {options.usedColors.map((color) => (
+                      <div key={`used-${color}`} className="DropdownOption Disabled">
+                        <span className="ColorCircle" style={{ backgroundColor: color }}></span>
+                        {color} (Already Used)
+                      </div>
+                    ))}
+                    {options.availableColors.map((color) => (
+                      <div key={`available-${color}`} className="DropdownOption" onClick={() => handleThemeSelection(color)}>
+                        <span className="ColorCircle" style={{ backgroundColor: color }}></span>
+                        {color}
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
             </div>
+
+            <button className="ModalSubmitButton" onClick={handleSubmit}>
+              Add Budget
+            </button>
+          </>
+        )}
+
+        {type === "edit" && (
+          <>
+            <p>As your budgets change, feel free to update your spending limits.</p>
+            <div className="FormGroup">
+              <label>Budget Category</label>
+              <input type="text" value={formData.category} disabled />
+            </div>
+
+            <div className="FormGroup">
+              <label>Maximum Spend</label>
+              <SearchField
+                type="icon-left"
+                placeholder="e.g. 2000"
+                value={formData.limit}
+                onChange={(e) =>
+                  setFormData({ ...formData, limit: e.target.value })
+                }
+              />
+            </div>
+
+            <div className="FormGroup">
+              <label>Theme</label>
+              <div className="ColorDropdown">
+              <div className="DropdownHeader">
+                  <div onClick={() => setDropdownOpen(!dropdownOpen)} style={{ display: "flex", alignItems: "center", width: "100%" }}>
+                    {formData.theme ? (
+                      <>
+                        <span className="ColorCircle" style={{ backgroundColor: formData.theme }}></span>
+                        {formData.theme}
+                      </>
+                    ) : (
+                      "Select Theme"
+                    )}
+                  </div>
+                  <img 
+                    src={Dropdowncross} 
+                    alt="Toggle Dropdown" 
+                    className={`DropdownCloseIcon ${dropdownOpen ? "visible" : "hidden"}`} 
+                    onClick={() => setDropdownOpen(!dropdownOpen)}
+                  />
+                </div>
+                {dropdownOpen && (
+                  <div className="DropdownList">
+                    {options.usedColors.map((color) => (
+                      <div key={`used-${color}`} className="DropdownOption Disabled">
+                        <span className="ColorCircle" style={{ backgroundColor: color }}></span>
+                        {color} (Already Used)
+                      </div>
+                    ))}
+                    {options.availableColors.map((color) => (
+                      <div key={`available-${color}`} className="DropdownOption" onClick={() => handleThemeSelection(color)}>
+                        <span className="ColorCircle" style={{ backgroundColor: color }}></span>
+                        {color}
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+            </div>
+
             <button className="ModalSubmitButton" onClick={handleSubmit}>
               Save Changes
             </button>
           </>
         )}
+
         {type === "delete" && (
           <>
-            <p>
-              Are you sure you want to delete this budget? This action cannot be
-              reversed, and all the data inside it will be removed forever.
-            </p>
-            <button className="ModalDeleteButton" onClick={handleDelete}>
-              Yes, Confirm Deletion
-            </button>
-            <button className="ModalCancelButton" onClick={onClose}>
-              No, Go Back
-            </button>
+            <p>Are you sure you want to delete this budget? This action cannot be reversed, and all the data inside it will be removed forever.</p>
+            <button className="ModalDeleteButton" onClick={handleDelete}>Yes, Confirm Deletion</button>
+            <button className="ModalCancelButton" onClick={onClose}>No, Go Back</button>
           </>
         )}
       </div>
